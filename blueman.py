6950a9258bcd21a156fe86437b6f6e82d903ef2b
Azul <azul.fierro22@gmail.com>
2026-09-07 21:43:32 -0700

    1st code delivery final

--- blueman.py
+++ blueman.py
@@ -6,27 +6,30 @@ def menu():
     print()
     print("1. Play")
     print("2. Exit")
+    
     option = int(input("Choose an option: "))
-
     return option
+
+
+def play():
+    word = random.choice(["python", "computer", "school"])
+    
+    print("Let's play Blueman!")
+    print("The mystery word has been selected.")
+    
+    letter = input("Guess a letter: ")
+
+    if letter in word:
+        print("Correct!")
+    else:
+        print("Wrong!")
 
 
 def main():
     option = menu()
 
     if option == 1:
-        print("Let's play Blueman!")
-        
-        word = random.choice(["python", "computer", "school"])
-        
-        print("The mystery word has been selected.")
-        
-        letter = input("Guess a letter: ")
-
-        if letter in word:
-            print("Correct!")
-        else:
-            print("Wrong!")
+        play()
 
     elif option == 2:
         print("Goodbye!")
